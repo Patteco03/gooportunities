c8392d728e29a3a7d4452b3c2942cec45b5adcee
Thiago Macedo <thiagodark1995@gmail.com>
2024-01-13 22:03:51 -0400

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "exemple/gooportunities/router"
+import (
+	"exemple/gooportunities/config"
+	"exemple/gooportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config.Init() failed: %v ", err)
+		return
+	}
+
+	// Initialize the router
 	router.Initialize()
 }
